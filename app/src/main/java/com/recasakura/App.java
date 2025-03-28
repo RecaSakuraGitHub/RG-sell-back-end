eac1f86ae216dcdfab3ec0902b6956073abda427
RecaSakura <recasakura@recasakura.com>
2025-03-28 22:59:57 +0800

    Test for merge different files.

--- app/src/main/java/com/recasakura/App.java
+++ app/src/main/java/com/recasakura/App.java
@@ -6,6 +6,7 @@ package com.recasakura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
