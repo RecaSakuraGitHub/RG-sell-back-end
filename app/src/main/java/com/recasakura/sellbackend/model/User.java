0b0eb3578a375d1b45bede57b93f3bc6aca80fac
RecaSakura <recasakura@recasakura.com>
2025-04-01 21:10:32 +0800

    feat(model): add User model with basic fiedls (id, name, email, phone)
    功能: 定義使用者的各項數據結構，包含(id, name, email, phone)，其中id為自動生成，無須在請求時傳入。

---
+++ app/src/main/java/com/recasakura/sellbackend/model/User.java
@@ -0,0 +1,45 @@
+package com.recasakura.sellbackend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+// table name = users
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(length = 10)
+    private String phone;
+
+    public User(){}
+    public User(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+    public Long getId() { return this.id; }
+    public String getName() { return this.name; }
+    public String getEmail() { return this.email; }
+    public String getPhone() { return this.phone; }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
