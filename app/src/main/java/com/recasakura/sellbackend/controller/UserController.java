842f7c5417767caa598a1dac2b14d13f18862335
RecaSakura <ian.lu1019@gmail.com>
2025-04-04 20:15:02 +0800

    refactor(controller): modify the method remove service logic from UserController
    重構: 將業務邏輯移交給service來做處理，因此重構大部分method。

--- app/src/main/java/com/recasakura/sellbackend/controller/UserController.java
+++ app/src/main/java/com/recasakura/sellbackend/controller/UserController.java
@@ -5,32 +5,40 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.recasakura.sellbackend.model.User;
-import com.recasakura.sellbackend.repository.UserRepository;
+import com.recasakura.sellbackend.model.user.*;
+import com.recasakura.sellbackend.repository.projection.UserProjection;
+import com.recasakura.sellbackend.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 @CrossOrigin(origins = "*")
 public class UserController {
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping
-    public ResponseEntity<String> createUser(@RequestBody User user) {
-        userRepository.save(user);
-        return ResponseEntity.ok("User: " + user.getName() + " is created.");
+    public ResponseEntity<String> createUser(@RequestBody UserCreateRequest userCreateRequest) {
+        UserResponse user = this.userService.createUser(userCreateRequest);
+        return ResponseEntity.ok("user: " + user + " is created.");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        UserResponse user = this.userService.getUserById(id);
+        return ResponseEntity.ok(user);
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> getUsers() {
-        return ResponseEntity.ok(userRepository.findAll());
+    public ResponseEntity<List<UserProjection>> getAllUsers() {
+        return ResponseEntity.ok(this.userService.getAllUsers());
     }
 }
