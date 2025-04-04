fd1b2c4abb463053fc177af9da3513b5ff7a41bc
RecaSakura <ian.lu1019@gmail.com>
2025-04-04 18:00:43 +0800

    feat(repository): add read method(email, phone)
    功能:
    1. 新增使用email或phone查詢功能。
    2. 新增判斷email或phone是否存在的功能。

--- app/src/main/java/com/recasakura/sellbackend/repository/UserRepository.java
+++ app/src/main/java/com/recasakura/sellbackend/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package com.recasakura.sellbackend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.recasakura.sellbackend.model.User;
+import com.recasakura.sellbackend.model.user.*;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhone(String phone);
+    Optional<User> findByEmailAndPhone(String email, String phone);
+    boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
+}
