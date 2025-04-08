75f80bda122e7d4c90260a9b6eb807ef119a7c90
RecaSakura <ian.lu1019@gmail.com>
2025-04-08 17:34:20 +0800

    feat(model): add UserDeleteRequest to hide after delete User's infomation

---
+++ app/src/main/java/com/recasakura/sellbackend/model/user/UserDeleteRequest.java
@@ -0,0 +1,17 @@
+package com.recasakura.sellbackend.model.user;
+
+public class UserDeleteRequest {
+    private String email;
+    private String phone;
+
+    public UserDeleteRequest() {}
+
+    public String getEmail() { return this.email; }
+    public String getPhone() { return this.phone; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
