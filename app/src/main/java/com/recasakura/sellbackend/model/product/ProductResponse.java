5c2420e097ca70df1b012f9e458d224e9567157d
RecaSakura <ian.lu1019@gmail.com>
2025-04-08 17:32:09 +0800

    feat(model): add ProductResponse to hide Product parameter

---
+++ app/src/main/java/com/recasakura/sellbackend/model/product/ProductResponse.java
@@ -0,0 +1,22 @@
+package com.recasakura.sellbackend.model.product;
+
+import java.math.BigDecimal;
+
+public class ProductResponse {
+    private String name;
+    private BigDecimal price;
+
+    public ProductResponse() {}
+    public ProductResponse(Product product) {
+        this.name = product.getName();
+        this.price = product.getPrice();
+    }
+    public String getName() { return this.name; }
+    public BigDecimal getPrice() { return this.price; }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
