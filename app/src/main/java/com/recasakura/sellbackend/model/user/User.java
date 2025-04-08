cec93391e6ff8abe5e52042a4e9f4baaf2a127d0
RecaSakura <ian.lu1019@gmail.com>
2025-04-08 17:33:34 +0800

    refactor(model): change GenerationType from AUTO to INDENTITY

--- app/src/main/java/com/recasakura/sellbackend/model/user/User.java
+++ app/src/main/java/com/recasakura/sellbackend/model/user/User.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 // table name = users
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
