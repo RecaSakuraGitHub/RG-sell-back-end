dccfc99c593f6326e7f78b3ed20b83ee9c5212d0
RecaSakura <ian.lu1019@gmail.com>
2025-04-08 17:38:36 +0800

    feat(repository): add multiple method
    功能:
    1. 添加findByName使用名稱找商品。
    2. 添加findAllBy回傳的是ProductProjection，防止Proudct隱私資訊洩漏。
    3. 添加existsByName使用名稱查找商品是否存在。

--- app/src/main/java/com/recasakura/sellbackend/repository/ProductRepository.java
+++ app/src/main/java/com/recasakura/sellbackend/repository/ProductRepository.java
@@ -1,7 +1,16 @@
 package com.recasakura.sellbackend.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.recasakura.sellbackend.model.Product;
+import com.recasakura.sellbackend.model.product.*;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+    List<ProductProjection> findAllBy();
+    boolean existsByName(String name);
+}
