f987c0306676c2520869a487a8bbcf1c9663916f
RecaSakura <ian.lu1019@gmail.com>
2025-04-08 17:31:29 +0800

    feat(model): add ProductRequest to hide Product parameter

---
+++ app/src/main/java/com/recasakura/sellbackend/model/product/ProductRequest.java
@@ -0,0 +1,19 @@
+package com.recasakura.sellbackend.model.product;
+
+import java.math.BigDecimal;
+
+public class ProductRequest {
+    private String name;
+    private BigDecimal price;
+
+    public ProductRequest() {}
+
+    public String getName() { return this.name; }
+    public BigDecimal getPrice() { return this.price; }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
