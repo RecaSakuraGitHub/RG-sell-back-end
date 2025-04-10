780e7bb76b7fd28eb5af79967a4d266f99ad7f64
RecaSakura <ian.lu1019@gmail.com>
2025-04-10 21:57:45 +0800

    feat(controller): add session to determine user permissions
    功能:
    1. 添加HttpSession session參數，並交由ProductService確認使用者是否有權更動產品資訊。
    2. 更動查詢產品參數從PathVariable Long id變成PathVariable String name。

--- app/src/main/java/com/recasakura/sellbackend/controller/ProductController.java
+++ app/src/main/java/com/recasakura/sellbackend/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.recasakura.sellbackend.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.recasakura.sellbackend.model.product.*;
 import com.recasakura.sellbackend.service.ProductService;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/products")
@@ -25,16 +29,27 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductResponse> createProducts(@RequestBody ProductRequest request) {
-        ProductResponse response = this.productService.createProduct(request);
+    public ResponseEntity<?> createProduct(@RequestBody ProductRequest request, HttpSession session) {
+        this.productService.checkAdmin(session);
+        this.productService.createProduct(request);
+        return ResponseEntity.ok(Map.of("message", request.getName() + " created"));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id, HttpSession session) {
+        this.productService.checkAdmin(session);
+        String name = this.productService.getProduct(id).getName();
+        this.productService.deleteProduct(id);
+        return ResponseEntity.ok(Map.of("message", name + " deleted"));
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Product> getProduct(@PathVariable String name, HttpSession session) {
+        this.productService.checkAdmin(session);
+        Product response = this.productService.getProduct(name);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductResponse> getProduct(@PathVariable Long id) {
-        ProductResponse response = this.productService.getProduct(id);
-        return ResponseEntity.ok(response);
-    }
     @GetMapping
     public ResponseEntity<List<ProductProjection>> getProducts() {
         return ResponseEntity.ok(this.productService.getProducts());
