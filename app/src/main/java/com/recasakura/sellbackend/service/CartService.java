5e890dbf321c22906c101cf14f7ad1e6190fcf98
RecaSakura <ian.lu1019@gmail.com>
2025-04-10 22:23:43 +0800

    feat(service): add CartService to handle cart service logice
    功能:
    1. 使用HttpSession session來存放使用者的購物車資訊。
    2. 提供addToCart來讓使用者可已添加product進入購物車。
    3. 提供getCart讓使用者可取得已添加進cart的product數量與金額。

---
+++ app/src/main/java/com/recasakura/sellbackend/service/CartService.java
@@ -0,0 +1,45 @@
+package com.recasakura.sellbackend.service;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.recasakura.sellbackend.model.cart.CartAddRequest;
+import com.recasakura.sellbackend.model.cart.CartItemResponse;
+import com.recasakura.sellbackend.model.product.Product;
+import com.recasakura.sellbackend.repository.ProductRepository;
+
+import jakarta.servlet.http.HttpSession;
+
+@Service
+public class CartService {
+    public final ProductRepository productRepository;
+    public CartService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void addToCart(CartAddRequest request, HttpSession session) {
+        Map<Long, Integer> cart = (Map<Long, Integer>) session.getAttribute("cart");
+        if (cart == null) {
+            cart = new HashMap<>();
+        }
+        cart.put(request.getProductId(), cart.getOrDefault(request.getProductId(), 0) + 1);
+        session.setAttribute("cart", cart);
+    }
+
+    public List<CartItemResponse> getCart(HttpSession session) {
+        Map<Long, Integer> cart = (Map<Long, Integer>) session.getAttribute("cart");
+        if (cart == null) return List.of();
+        return cart.entrySet().stream()
+            .map(entry -> {
+                Product product = this.productRepository.findById(entry.getKey()).orElse(null);
+                if (product == null) return null;
+                BigDecimal productPrice = product.getPrice();
+                return new CartItemResponse(product.getName(), entry.getValue(), productPrice.multiply(BigDecimal.valueOf(entry.getValue())));
+        }).collect(Collectors.toList());
+    }
+}
