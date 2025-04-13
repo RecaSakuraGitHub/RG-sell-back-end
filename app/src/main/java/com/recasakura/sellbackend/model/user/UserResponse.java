ba0489d678ab1405b601d516cc2fbf0c025f1131
RecaSakura <ian.lu1019@gmail.com>
2025-04-13 21:55:34 +0800

    feat(model): add new param String role to UserResponse

--- app/src/main/java/com/recasakura/sellbackend/model/user/UserResponse.java
+++ app/src/main/java/com/recasakura/sellbackend/model/user/UserResponse.java
@@ -5,17 +5,20 @@ public class UserResponse {
     private String name;
     private String email;
     private String phone;
+    private String role;
 
     public UserResponse(){}
     public UserResponse(User user) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.phone = user.getPhone();
+        this.role = user.getRole();
     }
 
     public String getName() { return this.name; }
     public String getEmail() { return this.email; }
     public String getPhone() { return this.phone; }
+    public String getRole() { return this.role; }
 
     public void setName(String name) {
         this.name = name;
@@ -25,5 +28,8 @@ public class UserResponse {
     }
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+    public void setRole(String role) {
+        this.role = role;
     }
 }
