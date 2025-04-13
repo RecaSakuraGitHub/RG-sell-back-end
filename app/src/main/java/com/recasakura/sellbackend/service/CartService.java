80a17706649e72e795a5372086086e9a594c2824
RecaSakura <ian.lu1019@gmail.com>
2025-04-13 22:05:16 +0800

    fix(service): fix addToCart whatever quantity request only add one item to cart

--- app/src/main/java/com/recasakura/sellbackend/service/CartService.java
+++ app/src/main/java/com/recasakura/sellbackend/service/CartService.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.recasakura.sellbackend.exception.ProductNotFoundException;
 import com.recasakura.sellbackend.model.cart.CartAddRequest;
 import com.recasakura.sellbackend.model.cart.CartItemResponse;
 import com.recasakura.sellbackend.model.product.Product;
@@ -23,26 +24,26 @@ public class CartService {
     }
 
     public void addToCart(CartAddRequest request, HttpSession session) {
-        Map<Long, Integer> cart = (Map<Long, Integer>) session.getAttribute("cart");
+        Map<String, Integer> cart = (Map<String, Integer>) session.getAttribute("cart");
         if (cart == null) {
             cart = new HashMap<>();
         }
-        if (this.productRepository.findById(request.getProductId()).orElse(null) == null) {
-            return;
+        if (!this.productRepository.existsByName(request.getName())) {
+            throw new ProductNotFoundException();
         }
-        cart.put(request.getProductId(), cart.getOrDefault(request.getProductId(), 0) + 1);
+        cart.put(request.getName(), cart.getOrDefault(request.getName(), 0) + request.getQuantity());
         session.setAttribute("cart", cart);
     }
 
     public List<CartItemResponse> getCart(HttpSession session) {
-        Map<Long, Integer> cart = (Map<Long, Integer>) session.getAttribute("cart");
+        Map<String, Integer> cart = (Map<String, Integer>) session.getAttribute("cart");
         if (cart == null) return List.of(new CartItemResponse("cart is empty", 0, BigDecimal.ZERO));
         return cart.entrySet().stream()
             .map(entry -> {
-                Product product = this.productRepository.findById(entry.getKey()).orElse(null);
+                Product product = this.productRepository.findByName(entry.getKey()).orElse(null);
                 if (product == null) return null;
                 BigDecimal productPrice = product.getPrice();
-                return new CartItemResponse(product.getName(), entry.getValue(), productPrice.multiply(BigDecimal.valueOf(entry.getValue())));
+                return new CartItemResponse(entry.getKey(), entry.getValue(), productPrice.multiply(BigDecimal.valueOf(entry.getValue())));
         }).collect(Collectors.toList());
     }
 }
